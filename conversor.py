3061900a28cefbd51e15317c660c790690fb942b
Neidys Betancourt <betancourtneidys@gmail.com>
2021-06-16 19:55:37 -0300

    Agregue un archivo .gitignore y varios países al conversor de monedas

--- conversor.py
+++ conversor.py
@@ -1,7 +1,37 @@
-pesos = input("¿Cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3693
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+Bienvenido al conversor de monedas 💰
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3693
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 160
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 20
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print("Ingresa una opción correcta por favor.")
